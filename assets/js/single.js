3e8bba9a3e5c1a8fc3b35d6ae1367ba36af7940d
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-05-04 08:45:02 -0400

    end 6.3.7, 30+ issues alert/link added

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
     
@@ -9,6 +10,11 @@ var getRepoIssues = function(repo) {
                 console.log(data);
                 //pass response data to DOM function
                 displayIssues(data);
+
+                //check if api has paginated issues
+                if (response.headers.get("link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
@@ -50,5 +56,17 @@ var displayIssues = function(issues) {
         issueContainerEl.appendChild(issueEl);
     }
 }
+
+var displayWarning = function(repo) {
+    //add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit: ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
 
 getRepoIssues("facebook/react");
