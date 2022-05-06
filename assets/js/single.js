f52e373555a1e232ccd82a45698299c43b1a42ed
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-05-06 11:57:50 -0400

    6.4.6 end, error handling and redirects added

--- assets/js/single.js
+++ assets/js/single.js
@@ -5,8 +5,12 @@ var limitWarningEl = document.querySelector("#limit-warning");
 var getRepoName = function() {
   var queryString = document.location.search;
   var repoName = queryString.split("=")[1];
-  getRepoIssues(repoName); 
-  repoNameEl.textContent = repoName;
+  if(repoName) {
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName); 
+  } else {
+    document.location.replace("./index.html");
+  }
 }
 
 var getRepoIssues = function(repo) {
@@ -27,8 +31,7 @@ var getRepoIssues = function(repo) {
       });
     }
     else {
-      console.log(response);
-      alert("There was a problem with your request!");
+      document.location.replace("./index.html");
     }
   });
 };
