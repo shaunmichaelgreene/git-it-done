69baf0f1772992363d6704170465f20c1dd82c77
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-05-06 12:10:45 -0400

    6.5.3 start initial commit

--- assets/js/single.js
+++ assets/js/single.js
@@ -3,15 +3,20 @@ var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoName = function() {
+  // grab repo name from url query string
   var queryString = document.location.search;
   var repoName = queryString.split("=")[1];
-  if(repoName) {
+
+  if (repoName) {
+    // display repo name on the page
     repoNameEl.textContent = repoName;
-    getRepoIssues(repoName); 
+
+    getRepoIssues(repoName);
   } else {
+    // if no repo was given, redirect to the homepage
     document.location.replace("./index.html");
   }
-}
+};
 
 var getRepoIssues = function(repo) {
   // format the github api url
@@ -29,8 +34,8 @@ var getRepoIssues = function(repo) {
           displayWarning(repo);
         }
       });
-    }
-    else {
+    } else {
+      // if not successful, redirect to homepage
       document.location.replace("./index.html");
     }
   });
@@ -49,12 +54,11 @@ var displayIssues = function(issues) {
     issueEl.classList = "list-item flex-row justify-space-between align-center";
     issueEl.setAttribute("href", issues[i].html_url);
     issueEl.setAttribute("target", "_blank");
-    
+
     // create span to hold issue title
     var titleEl = document.createElement("span");
     titleEl.textContent = issues[i].title;
-    
-  
+
     // append to container
     issueEl.appendChild(titleEl);
 
@@ -64,14 +68,13 @@ var displayIssues = function(issues) {
     // check if issue is an actual issue or a pull request
     if (issues[i].pull_request) {
       typeEl.textContent = "(Pull request)";
-    }
-    else {
+    } else {
       typeEl.textContent = "(Issue)";
     }
 
     // append to container
     issueEl.appendChild(typeEl);
-  
+
     // append to the dom
     issueContainerEl.appendChild(issueEl);
   }
@@ -90,4 +93,5 @@ var displayWarning = function(repo) {
   // append to warning container
   limitWarningEl.appendChild(linkEl);
 };
+
 getRepoName();
